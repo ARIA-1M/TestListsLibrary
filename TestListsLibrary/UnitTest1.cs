a1e58329b25f8386b0bd32e42dca2a34830ca342
ARIA-1M <perevezencevamaria32@gmail.com>
2026-04-02 22:41:35 +0300

    доделаные тесты

--- TestListsLibrary/UnitTest1.cs
+++ TestListsLibrary/UnitTest1.cs
@@ -1,11 +1,61 @@
-﻿namespace TestListsLibrary
+﻿using static WorkingListsLibrary.ClassLibrary<string>;
+
+namespace TestListsLibrary
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        public List<string> list = new List<string> { "Мария", "Мери", "Маша", "Арина", "Настя", "Аня" };
+        string item = "Маршал";
+        string itemNull = null;
+        public List<string> listNull = new List<string>();
+
+
+       [Fact]
+        public void AddListCorrect()//Правильное добавление в лист
         {
+            ListHelper.AddList(list, item);
+            Assert.Equal(7, list.Count);
+        }
+
+        [Fact]
+        public void AddListError()//Ошибка добавление в лист
+        {
+            Assert.Throws<ArgumentNullException>(() => ListHelper.AddList(list, itemNull));
+        }
+
+
+        [Fact]
+        public void DeletListCorrect()//Правильное удаление из листа
+        {
+            ListHelper.DeletList(list, item);
+            Assert.Equal(6, list.Count);
+        }
+
+        [Fact]
+        public void DeletListError()//Ошибка удаление из листа
+        {
+            Assert.Throws<ArgumentNullException>(() => ListHelper.DeletList(list, itemNull));
+        }
+
+        [Fact]
+        public void SearchListCorrect()//Поиск в листе по подстроке
+        {
+            string input = "Ма";
+            var stringReader = new StringReader(input);
+            Console.SetIn(stringReader);
+            var exception = Record.Exception(() => ListHelper.SearchList(list));
+            Assert.Null(exception);
 
         }
+
+        [Fact]
+        public void SearchListError()//Ошибка поиска в листе
+        {
+            string input = "Ма";
+            var stringReader = new StringReader(input);
+            Console.SetIn(stringReader);
+            Assert.Throws<ArgumentNullException>(() => ListHelper.SearchList<string>(null));
+        }
+
     }
 }
